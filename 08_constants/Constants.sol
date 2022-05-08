d9024ba2af440bbb6fc306e2c15b6090fdd6a71e
Mikael <dev.kov.mtl@gmail.com>
2022-05-08 16:44:15 -0400

    Constant

---
+++ 08_constants/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Constants {
+    // declare constant we can save gas
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
