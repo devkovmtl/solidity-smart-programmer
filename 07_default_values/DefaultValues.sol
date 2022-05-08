1d4f8d633b08097aa6b5fed683665a00ffe5d49b
Mikael <dev.kov.mtl@gmail.com>
2022-05-08 16:38:48 -0400

    default values

---
+++ 07_default_values/DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DefaultValues {
+    bool public b; // default is false
+    uint public u; // default is 0
+    int public i; // default is 0
+    address public a; // default is 0x0000000000000000000000000000000000000000
+    bytes32 public b32; // default is 0x0000000000000000000000000000000000000000000000000000000000000000
+}
