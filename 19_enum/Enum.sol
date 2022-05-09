7e16ef5863bb806ca1a67f220ea9fda94fd6034c
Mikael <dev.kov.mtl@gmail.com>
2022-05-09 09:17:23 -0400

    Enum

---
+++ 19_enum/Enum.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// express different values
+contract Enum {
+    enum Status {
+        None,
+        Pending,
+        Shipped,
+        Completed,
+        Rejected,
+        Cancelled
+    }
+
+    Status public status;
+
+    struct Order {
+        address buyer;
+        Status status;
+    }
+
+    Order[] public orders;
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) external {
+        status = _status;
+    }
+
+    function ship() external  {
+        status = Status.Shipped;
+    }
+
+    function reset() external {
+        delete status; // reset to default value first items
+    }
+}
