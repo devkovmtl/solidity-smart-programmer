d1db98e4c7643f7fd1e3b093e064169dde63c66f
Mikael <dev.kov.mtl@gmail.com>
2022-05-11 14:15:14 -0400

    Aggregate multiple query into single function call multicall

---
+++ 52_MultiCall/MultiCall.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+// We want to make query to a contract call multiple times.
+// call multiple function 
+contract TestMultiCall {
+    function func1() external view returns (uint, uint) {
+        return (1, block.timestamp);
+    }
+
+    function func2() external view returns (uint, uint) {
+        return (2, block.timestamp);
+    }
+
+    // data to instruct to call func1 and func2
+    function getData1() external pure returns (bytes memory) {
+        return abi.encodeWithSelector(this.func1.selector);
+    }
+
+    function getData2() external pure returns (bytes memory) {
+        return abi.encodeWithSelector(this.func2.selector);
+    }
+}
+
+contract MultiCall {
+    function multicall(address[] calldata targets, bytes[] calldata data) external view returns(bytes[] memory){
+        require(targets.length == data.length, "targets length != data length");
+        bytes[] memory results = new bytes[](data.length);
+        for(uint i; i < targets.length; i++) {
+           (bool success, bytes memory result) = targets[i].staticcall(data[i]);
+           require(success, "call failed");
+           results[i] = result;
+        }
+        return results;
+    }
+}
